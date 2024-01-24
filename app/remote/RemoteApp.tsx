4973914eba1fba038a6484fe9631d50a3faabb88
Aarón <146104238+aaronhernandez-stripe@users.noreply.github.com>
2024-01-24 14:33:49 +0000

    feat: Add missing commas to Button and NoProps, remove unnecessary code from RemoteApp.tsx, update message text in Box component and fix syntax error in NoProps component

--- app/remote/RemoteApp.tsx
+++ app/remote/RemoteApp.tsx
@@ -9,13 +9,13 @@ import type {
 
 // const Button = "Button" as any;
 const Button = createRemoteReactComponent<"Button", ButtonProps, true>(
-  "Button"
+  "Button",
 );
 const Box = createRemoteReactComponent<"Box", BoxProps, true>("Box");
 // const NoProps = "NoProps" as any;
 // Still has children
 const NoProps = createRemoteReactComponent<"NoProps", NoPropsProps, false>(
-  "NoProps"
+  "NoProps",
 );
 const TextField = createRemoteReactComponent<
   "TextField",
@@ -35,15 +35,8 @@ export function RemoteApp({
   return (
     <Box withBorder>
       ---- From remote ui iframe: ----
-      <Box>Message {message}</Box>
-      <Box
-        children={[
-          <Box key="1">Children array test!!</Box>,
-          <Box key="2">Children 2</Box>,
-        ]}
-      />
-      <NoProps />
-      <TextField label="1: string label" />
+      <Box>Message from host: {message}</Box>
+      <NoProps children={<>This should fail</>} />
       <Button
         onPress={async () => {
           const message = await getMessage();
